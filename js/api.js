26b9693d7185fea1b6cebc1c51ec8bbceefa166a
Dush0206 <dushyanth.n0206@gmail.com>
2026-06-26 22:00:01 +0530

    feat: working task manager with MongoDB Atlas

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 // ===== API Configuration =====
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'http://127.0.0.1:5000/api';
 
 const getToken = () => localStorage.getItem('token');
 
